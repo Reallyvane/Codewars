55a06e69bc7f12a626048d899776a6457678f279
Vanessa <contact.nesdevs@gmail.com>
2023-02-03 02:29:14 -0500

    First Commit

---
+++ 8 kyu/Square(n) Sum/index.js
@@ -0,0 +1,17 @@
+/*
+
+Description:
+
+Complete the square sum method so that it squares each number passed into it and then sums the results together.
+
+For example: squareSum([1, 2, 2]) should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+*/
+
+function squareSum(numbers) {
+  return numbers
+    .map((x) => x * x) // creates a new array
+    .reduce((total, x) => {
+      return total + x;
+    }, 0); // reduce array to a single value
+}
